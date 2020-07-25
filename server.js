972b067b3b82ba686f6a6ef18536601acdd4f2d9
tejaattainu <tejspvg@gmail.com>
2020-07-25 18:18:16 +0530

    minor updates

--- server.js
+++ server.js
@@ -12,11 +12,7 @@ const app = express();
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-    allowedHeaders: ["Content-Type"]
-  })
+  cors()
 );
 
 app.use(apiRoutes);
