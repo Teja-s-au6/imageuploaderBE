6638183c7f8ca95af65fdb8a7681b5fd3a14d7ae
tejaattainu <tejspvg@gmail.com>
2020-07-26 08:00:19 +0530

    minor updates

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const userRoutes = require('./routes/userRoutes');
 
 // Init
 const app = express();
-app.use(express.urlencoded({ extended : true }));
+app.use(express.urlencoded({ extended : false }));
 app.use(express.json())
 app.use(express.json());
 app.use(cors(
